dd192754fe98cfd2d9953fe256adcccac53a7d15
adamocallaghan <adam.ocallaghan@yahoo.ie>
2023-08-11 15:25:06 +0100

    Added some tests

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -137,6 +137,14 @@ contract Raffle is VRFConsumerBaseV2 {
         return i_entranceFee;
     }
 
+    function getRaffleState() external view returns (RaffleState) {
+        return s_raffleState;
+    }
+
+    function getPlayer(uint256 indexOfPlayer) external view returns (address) {
+        return s_players[indexOfPlayer];
+    }
+
     // CEI: CHECKS, EFFECTS, INTERACTIONS
     function fulfillRandomWords(
         uint256 /* requestId */,
